37143ea9439a8b6b5c84dfa5689a6f62737d450c
Darsana P G <darsana.pg@elsyslabs.com>
2022-09-20 03:20:28 +0530

    add one screen named 'home' for start the app

--- screens/navigation.js
+++ screens/navigation.js
@@ -3,13 +3,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import mymovies from '../screens/mymovies';
 import Details from './details';
+import home from './home';
+import drawer from './drawer';
 
 const Stack = createNativeStackNavigator();
 export default function Navigation() {
 return(
 <NavigationContainer>
 <Stack.Navigator initialRouteName="Home">
-{/* <Stack.Screen name="drawer" component={drawer} options={{headerShown:false}}/>   */}
+<Stack.Screen name="drawer" component={drawer} options={{headerShown:false}}/> 
+<Stack.Screen name="home" component={home}/>
 <Stack.Screen name="Mymovies" component={mymovies}/>
 <Stack.Screen name="Details" component={Details}/>
 </Stack.Navigator>
