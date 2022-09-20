7fb13c724a8240548a1ed5d00b24129206fae45a
Darsana P G <darsana.pg@elsyslabs.com>
2022-09-20 13:31:23 +0530

    add plugin in babel.config and add one drawer and home screen

--- screens/drawer.js
+++ screens/drawer.js
@@ -4,6 +4,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import mymovies from './mymovies';
 import Details from './details';
+import home from './home';
 
 
 const Drawer = createDrawerNavigator();
@@ -11,6 +12,7 @@ const Drawer = createDrawerNavigator();
 export default function drawer() {
   return (
       <Drawer.Navigator initialRouteName="home" options={{headerShown: false}}>
+        <Drawer.Screen name="home" component={home} />
         <Drawer.Screen name="mymovies" component={mymovies} />
         <Drawer.Screen name="Details" component={Details} />    
       </Drawer.Navigator>
--- screens/home.js
+++ screens/home.js
@@ -1,59 +1,10 @@
 import * as React from 'react';
 import { View, TouchableOpacity, StyleSheet,Text,ImageBackground, StatusBar} from 'react-native';
-// import {horizontalScale,verticalScale} from './utils/scale'
-import { useNavigation } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
 
 export default function home(props) {
-  const nav = useNavigation();
-  return (
+  return(
     <View>
-      {/* <View style={styles.container}>
-    <ImageBackground source={require('../assets/note.png')} resizeMode="cover" style={styles.image}>
-    </ImageBackground>
-  </View> */}
-      <SafeAreaView>
-        <StatusBar backgroundColor={'black'}></StatusBar>
-      </SafeAreaView>
-      <View>   
-        <TouchableOpacity style={styles.button}  onPress={() => nav.navigate('main')}>
-   <Text style={styles.text}>start</Text>
-    </TouchableOpacity>
+      <ImageBackground source={require('../assets/movie.png')}/>
     </View>
-</View>
-  );
+  )
 }
-const styles = StyleSheet.create({
-  button: {
-    // width: horizontalScale(180),
-    // height: verticalScale(55),
-    borderRadius: 10,
-    backgroundColor: 'grey',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop:250,
-    marginLeft:130
-  },
-  text: {
-    color: 'black',
-    // fontSize: verticalScale(36),
-    fontWeight: 'bold',
-    fontFamily: 'Roboto',
-  },
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center"
-  },
-  // text: {
-  //   color: "white",
-  //   fontSize: 42,
-  //   lineHeight: 84,
-  //   fontWeight: "bold",
-  //   textAlign: "center",
-  //   backgroundColor: "#000000c0"
-  // }
-});
